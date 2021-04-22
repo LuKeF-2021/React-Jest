5f4c12e88affae41d4db5cdaacea0b214df8658f
aadil-moghal <aadilmoghal@gmail.com>
2021-04-22 12:46:36 +0100

    added buttons

--- car-garage/src/Webpage/CreateGarage.jsx
+++ car-garage/src/Webpage/CreateGarage.jsx
@@ -12,6 +12,12 @@ const CreateGarage = () => {
             <input name="ID" type="text"></input>
             <label>Name: </label>
             <input name="garageName" type="text"></input>
+            <br></br>
+            <button class="btn" id="add-garage">Add Garage</button>
+            <button class="btn" id="delete-garage">Delete Garage</button>
+            <button class="btn" id="show-garages">Show All Garage</button>
+            <button class="btn" id="find-garages-by-id">Find Garage By ID</button>
+            <button class="btn" id="update-garage">Update Garage</button>
             </form>
         </>
     );
