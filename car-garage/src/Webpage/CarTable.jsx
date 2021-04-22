dabc18fd3a94217114b8564aaa8d3af76356171f
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2021-04-22 11:31:03 +0100

    car table layout with placeholder data

--- car-garage/src/Webpage/CarTable.jsx
+++ car-garage/src/Webpage/CarTable.jsx
@@ -1,25 +1,37 @@
 import CarTableRow from "./CarTableRow";
+import React, { useEffect, useState } from "react";
 
 const CarTable = () => {
 	const Placeholder = (e) => {
 		e.preventDefault();
 	};
 
-	const car = () => {
-		//car api stuff
-	};
-
-	// DATA LAYOUT FROM API
-	// [
-	//     {
-	//         colour: "string",
-	//         doors: 0,
-	//         id: 0,
-	//         make: "string",
-	//         model: "string",
-	//         name: "string",
-	//     },
-	// ];
+	const car = [
+		{
+			colour: "red",
+			doors: 4,
+			id: 1,
+			make: "tesla",
+			model: "model 3",
+			name: "flash",
+		},
+		{
+			colour: "blue",
+			doors: 2,
+			id: 2,
+			make: "jaguar",
+			model: "XJ",
+			name: "shreddies",
+		},
+		{
+			colour: "grey",
+			doors: 4,
+			id: 3,
+			make: "bmw",
+			model: "x27",
+			name: "haydon",
+		},
+	];
 
 	return (
 		<div>
@@ -31,8 +43,20 @@ const CarTable = () => {
 					<th>make</th>
 					<th>model</th>
 					<th>name</th>
-                    <th>garage</th>
+					<th>garage</th>
 				</tr>
+
+				{car.map((item) => (
+					<tr>
+						<th>{item.colour}</th>
+						<th>{item.doors}</th>
+						<th>{item.id}</th>
+						<th>{item.make}</th>
+						<th>{item.model}</th>
+						<th>{item.name}</th>
+						<th>{item.garage}</th>
+					</tr>
+				))}
 
 				<CarTableRow />
 			</table>
