ed7e0cf744b20c8f3d43eba767326444a39ccc23
luke-foster <lukefoster2011@gmail.com>
2021-04-23 10:30:21 +0100

    added create functionality back

--- car-garage/src/Webpage/CreateCar.jsx
+++ car-garage/src/Webpage/CreateCar.jsx
@@ -3,11 +3,42 @@ import axios from "axios";
 import React from "react";
 import CarTable from './CarTable';
 
+const Placeholder = (e) => {
+	e.preventDefault();
+};
+
 
 const CreateCar = () => {
 
 const header = { "Access-Control-Allow-Origin": "*" };
 const baseURL = "http://18.133.181.100:9092";
+
+	const [carColour, setCarColour] = useState("");
+	const [carDoors, setCarDoors] = useState(0);
+	const [carGarageId, setCarGarageId] = useState(0);
+	const [carMake, setCarMake] = useState("");
+	const [carModel, setCarModel] = useState("");
+	const [carName, setCarName] = useState("");
+
+const CreateCarButton = (carObj) => {
+	return axios
+		.post(`${baseURL}/car/create`, carObj, { header })
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((error) => {
+			console.error(error);
+		});
+};
+
+const carObj = {
+	colour: carColour,
+	doors: carDoors,
+	garage: {id:carGarageId},
+	make: carMake,
+	model: carModel,
+	name: carName,
+};
 
  //set our data
  const [info, setInfo] = useState([]);
@@ -45,6 +76,85 @@ const baseURL = "http://18.133.181.100:9092";
  }else{
 
  return(
+	 <>
+	 <h2>Add Car</h2>
+
+			<form onSubmit={Placeholder}>
+				<label>Colour: </label>
+				<input
+					name="carColour"
+					type="text"
+					value={carColour}
+					onChange={(e) => setCarColour(e.target.value)}
+				></input>
+
+
+				<label>Doors: </label>
+				<input
+					name="carDoors"
+					type="text"
+					value={carDoors}
+					onChange={(e) => setCarDoors(e.target.value)}
+				></input>
+
+                <label>Garage ID: </label>
+				<input
+					name="carGarageId"
+					type="text"
+					value={carGarageId}
+					onChange={(e) => setCarGarageId(e.target.value)}
+				></input>
+
+
+
+				<label>Make: </label>
+				<input 
+                name="carMake" 
+                type="text" 
+                value={carMake} 
+                onChange={(e) => setCarMake(e.target.value)}
+                ></input>
+				
+                
+                
+                <label>Model: </label>
+				<input
+					name="carModel"
+					type="text"
+					value={carModel}
+					onChange={(e) => setCarModel(e.target.value)}
+				></input>
+
+
+                
+
+
+				<label>Name: </label>
+				<input 
+                name="carName" 
+                type="text" 
+                value={carName} 
+                onChange={(e) => setCarName(e.target.value)}
+                ></input>
+
+
+				<br></br>
+
+
+				<button className="btn" id="add-car" onClick={() => CreateCarButton(carObj)}>
+					Add Car
+				</button>
+				<button className="btn" id="find-car-by-name">
+					Find Car By Name
+				</button>
+				<button className="btn" id="show-cars">
+					Show All Cars
+				</button>
+				<button className="btn" id="find-car-by-id">
+					Find Car By ID
+				</button>
+				
+			</form> 
 	 <table>
 		 <thead>
 			 <tr>
@@ -68,6 +178,7 @@ const baseURL = "http://18.133.181.100:9092";
 
 		 </tbody>
 	 </table>
+	 </>
  )
 		 }
 }
